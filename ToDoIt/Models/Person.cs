83d2d249ba2475cfa1466d0b366beebde9f0b680
erch47 <erik.chr1@hotmail.se>
2022-09-29 14:30:18 +0200

    Person class finished

---
+++ ToDoIt/Models/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoIt.Models
+{
+    public class Person
+    {
+        private readonly int id;
+        private string firstName;
+        private string lastName;
+
+
+        public Person(int id)
+        {
+            this.id = id;
+        }
+
+
+        public Person(int id, string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+        }
+
+
+        public int PersonId
+        {
+            get { return id; }
+        }
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set 
+            {
+                if (value != null && value.Length > 0)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+            }              
+        }
+
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+            }
+        }
+    }
+}
